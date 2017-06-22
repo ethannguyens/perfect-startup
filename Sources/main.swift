84da207a4e0b2c30de648a86676997ba1c9c7799
Ethan <ethan.nguyens@gmail.com>
2017-06-22 20:40:00 +0100

    add swift server

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,30 @@
-print("Well hi Ethan Nguyen!")
+
+import PerfectLib
+import PerfectHTTP
+import PerfectHTTPServer
+ 
+// Create HTTP server.
+let server = HTTPServer()
+ 
+// Register your own routes and handlers
+var routes = Routes()
+routes.add(method: .get, uri: "/", handler: {
+        request, response in
+        response.setHeader(.contentType, value: "text/html")
+        response.appendBody(string: "<html><title>Ethan Nguyen!</title><body>Hello, world!</body></html>")
+        response.completed()
+    }
+)
+ 
+// Add the routes to the server.
+server.addRoutes(routes)
+ 
+// Set a listen port of 8181
+server.serverPort = 8181
+ 
+do {
+    // Launch the HTTP server.
+    try server.start()
+} catch PerfectError.networkError(let err, let msg) {
+    print("Network error thrown: \(err) \(msg)")
+}
